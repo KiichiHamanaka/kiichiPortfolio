477ea68d70279b50e00855eaf234d252a131308a
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2020-12-23 19:20:11 +0900

    first commit

---
+++ components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- TODO:Wordartで最高にいかしたロゴ -->
+    <h1 class="title">kiichiPortfolio</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+
+</style>
